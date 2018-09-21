2ad7c5691b5a153f88f843bf69f957b065184894
Nina Zakharenko <nzakharenko@gmail.com>
2018-09-20 17:00:18 -0700

    Use str instead of unicode for python 3, add self to vars, use an f string

--- hello/models.py
+++ hello/models.py
@@ -5,6 +5,6 @@ class LogMessage(models.Model):
     message = models.CharField(max_length=300)
     log_date = models.DateTimeField("date logged")
 
-    def __unicode__(self):
+    def __str__(self):
         """Returns a string representation of a message."""
-        return "'" + self.text + "' logged on " + log_date.strftime('%A, %d %B, %Y at %X')
+        return f"'{self.message}' logged on {self.log_date.strftime('%A, %d %B, %Y at %X')}"
