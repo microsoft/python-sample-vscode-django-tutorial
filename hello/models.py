2871c2c02512935ba3c7d49528ed23e60fe2ab0a
Kraig Brockschmidt <kraigb@microsoft.com>
2018-12-05 11:02:47 -0800

    Fix linter error on strftime https://github.com/Microsoft/vscode-docs/issues/2252

--- hello/models.py
+++ hello/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class LogMessage(models.Model):
     message = models.CharField(max_length=300)
@@ -6,4 +7,5 @@ class LogMessage(models.Model):
 
     def __str__(self):
         """Returns a string representation of a message."""
-        return f"'{self.message}' logged on {self.log_date.strftime('%A, %d %B, %Y at %X')}"
+        date = timezone.localtime(self.log_date)
+        return f"'{self.message}' logged on {date.strftime('%A, %d %B, %Y at %X')}"
